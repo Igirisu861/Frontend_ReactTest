53a769cc803e29122e2a65ef2f2a587beb176941
Igirisu861 <d.r.g.1835@gmail.com>
2024-10-04 21:45:20 -0600

    app test básica

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Linking, Image, TouchableOpacity, TouchableHighlight } from 'react-native';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 export default function App() {
+
+  const pressLink = () => {
+    Linking.openURL('https://youtu.be/S-SnfN8Gn3I?si=D4h3wm01K5Uxz9RK');
+  };
+
+
   return (
     <View style={styles.container}>
-      <Text style={styles.textColor}>Hello everyone, my name is Markiplier </Text>
+      
+      <Text style={styles.textTitle} numberOfLines={2}>Hello, everybody, my name is Welcome </Text>
+      <Text style={styles.textHeader}>What??</Text> 
+      <TouchableHighlight>
+        <Image 
+            source={require('./assets/biteof87.gif')}
+            style={{width: 300, height: 300}}
+        />
+      </TouchableHighlight>
+      
+      <Text style={styles.textColor} numberOfLines={3} onPress={pressLink}>was that the bite of 87?</Text>
+
+      <TouchableOpacity>
+        <Image
+          source={require('./assets/drawingMark.jpeg')}
+          style={{width:200, height: 200}}
+        />
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -19,5 +42,16 @@ const styles = StyleSheet.create({
   },
   textColor:{
     color: 'white'
+  },
+  textHeader:{
+    fontSize: 30,
+    color: 'red'
+  },
+  textTitle:{
+    fontSize: 40,
+    color: 'white',
+    textAlign: 'center',
   }
+
+
 });
