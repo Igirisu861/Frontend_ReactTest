787030b1ad65dd11d52aa9daa5960d8de96c85f8
Igirisu861 <d.r.g.1835@gmail.com>
2024-10-15 20:24:19 -0600

    update flex

--- App.js
+++ App.js
@@ -36,7 +36,9 @@ export default function App(){
             }}
           />
         </Stack.Navigator>
-      </NavigationContainer>
+      </NavigationContainer>      
     </>
+    
+      
   )
 }
--- screens/Home.js
+++ screens/Home.js
@@ -1,18 +1,27 @@
 
 import React from 'react'
-import {SafeAreaView, Image, Text} from 'react-native'
+import { useState } from 'react';
+import {SafeAreaView, Image, Text, View, Modal, StyleSheet, Pressable, Alert} from 'react-native'
 
 export default function Home(){
+    const [modalVisible, setModalVisible] = useState(false);
     return(
         <>
-            <SafeAreaView style={{alignItems: 'center'}}>
-                <Text style={{fontSize: 40}}>Sata Andagi</Text>
+            <SafeAreaView style={{alignItems: 'center', backgroundColor: 'black', flex:1, flexDirection:'column'}}>
+                <Text style={{fontSize: 40, color: 'white', marginBottom: 10}}>Sata Andagi</Text>
                 <Image
                     source={{uri: 'https://c4.wallpaperflare.com/wallpaper/42/974/347/ayumu-azumanga-daioh-great-leader-osaka-anime-azumanga-hd-art-wallpaper-preview.jpg'}}
-                    style={{width:500,height:500,marginRight:10}}
-                    
+                    style={{width:300,height:300,marginRight:10, borderRadius: 20}}
                 />
-                
+
+                <View
+                    style={{
+                        backgroundColor: "#000000c0",
+                        width: 100,
+                        height: 100
+                    }}>
+                    <Text>a</Text>
+                </View>
             </SafeAreaView>
         </>
     )
